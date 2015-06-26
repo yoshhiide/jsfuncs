e520ac176b1b8b70d04f5526fb6f54e9196a7e44
yoshihide <nabeyama@outlook.com>
2015-06-26 14:36:33 +0900

    Create checkRandom.js

---
+++ checkRandom.js
@@ -0,0 +1,17 @@
+console.log(checkRandom(100, 1, 4));
+/**
+ * 乱数が偏らないか自分で確認する為の関数
+ */
+function checkRandom(repeatCnt, lo, hi){
+  var stok = {}, cnt = 0;
+  return (function rept(stok, cnt){
+    if(cnt >= repeatCnt){
+      return stok;
+    }
+    var rdm = parseInt(Math.floor(Math.random() * hi), 10) + lo;
+    stok[rdm] = stok[rdm] ? stok[rdm] + 1 : 1;
+    cnt++;
+    return rept(stok, cnt);
+  })(stok, cnt);
+}
+
